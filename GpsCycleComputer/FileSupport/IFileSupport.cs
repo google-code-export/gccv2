d3bf482c52f65a9b9f4d9a357e21bf2b3a9fb940
andrzej.grzesik <andrzej.grzesik@1a90c7ba-994b-11dd-92ee-791284ec580f>
2010-06-20 20:27:08 +0000

    Extracted loading data from files into separate classes

---
+++ GpsCycleComputer/FileSupport/IFileSupport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+
+namespace GpsSample.FileSupport
+{
+    interface IFileSupport
+    {
+        bool Load(string filename, 
+            // allocated vector size (for dataX/Y/T)
+            int vector_size,          
+            // x and y realive to origin, in metres, t in sec ralative to start
+            ref float[] dataLat, ref float[] dataLong, ref  Int32[] dataT, 
+            out int data_size);
+    }
+}
